477a415b5339f9c72030ee87e12626db318bc3b5
xym <727347070@qq.com>
2019-03-20 16:56:09 +0800

    获取用户信息接口

---
+++ application/api/controller/User.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * User: may
+ * Date: 2019/3/20
+ * Time: 下午4:53
+ */
+namespace app\api\controller;
+
+use Twig\Token;
+
+class User extends Token
+{
+    public function userList()
+    {
+        //查询所有用户信息
+
+    }
+}
